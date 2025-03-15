d5f17b492171fa123dd59da7afb18ac0fac4a58f
Yuliia Hurska <yuliahrska07@gmail.com>
2025-03-15 02:04:27 +0200

    add views for movies and tvshows

--- movies/views.py
+++ movies/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.template import loader
+
+
+def home(request):
+    return render(request, "main.html")
 
 # Create your views here.
+
+def movies(request):
+    return render(request, "movies/movies.html")
