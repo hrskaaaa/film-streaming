2e2e10fd6e61120aa2e95aefe16cccf2eb29ca73
Yuliia Hurska <yuliahrska07@gmail.com>
2025-03-15 02:12:31 +0200

    create views for login and showing user profile

--- users/views.py
+++ users/views.py
@@ -1,8 +1,31 @@
-from django.urls import path
-from . import views
 
+from django.shortcuts import render,redirect
+from django.contrib.auth import login, authenticate, logout
+from django.contrib.auth.models import User
 # Create your views here.
 
-urlpatterns = [
-    
-]
+
+def loginPage(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+
+        try:
+            user = User.objects.get(username=username)
+        except:
+            print('Username does not exist')
+# if it would find the username that match the username and password that match that user it will return this user
+        user = authenticate(request, username=username, password=password)
+
+# if user exist login func will create a session for this user in database and it will add this session to browser cookies
+        if user is not None:
+            login(request, user)
+            return redirect('home')
+        else:
+            print("Username OR password is incorrect ")
+
+    return render(request, 'users/login_register.html')
+
+
+def profile(request):
+    return render(request, 'users/user-profile.html')
