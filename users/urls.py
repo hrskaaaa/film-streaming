59d2017261e276abcb87d5e4e021cf8ed814dbf4
Yuliia Hurska <yuliahrska07@gmail.com>
2025-03-16 23:01:39 +0200

    create url for registration

--- users/urls.py
+++ users/urls.py
@@ -5,8 +5,8 @@ from . import views
 
 urlpatterns = [
     path('login/', views.loginUser, name="login"),
-        path('logout/', views.logoutUser, name="logout"),
-
+    path('logout/', views.logoutUser, name="logout"),
+    path('register/', views.registerUser, name="register"),
 
     path('', views.profile, name="profile")
 ]
