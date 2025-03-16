aea7e0eee8ec1976645d4b12825435ca727e79b1
Yuliia Hurska <yuliahrska07@gmail.com>
2025-03-16 18:19:48 +0200

    refresh files

--- popcorn/urls.py
+++ popcorn/urls.py
@@ -20,5 +20,5 @@ from django.urls import include, path
 urlpatterns = [
     path("", include("movies.urls")),
     path('admin/', admin.site.urls),
-    path('user/', include('users.urls')),
+    path('', include('users.urls')),
 ]
--- users/views.py
+++ users/views.py
@@ -2,10 +2,14 @@
 from django.shortcuts import render,redirect
 from django.contrib.auth import login, authenticate, logout
 from django.contrib.auth.models import User
+from django.contrib import messages
 # Create your views here.
 
 
 def loginUser(request):
+
+    if request.user.is_authenticated:
+        return redirect('home')
     if request.method == 'POST':
         username = request.POST['username']
         password = request.POST['password']
