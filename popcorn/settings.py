f7352d61e70566ddf2dd364cbcd400243d739dc1
Yuliia Hurska <yuliahrska07@gmail.com>
2025-05-28 16:31:22 +0300

    settings for sending emaild

--- popcorn/settings.py
+++ popcorn/settings.py
@@ -117,6 +117,13 @@ USE_I18N = True
 USE_TZ = True
 
 
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'filmspopcornplus@gmail.com'
+EMAIL_HOST_PASSWORD = 'cxumtqgwjmwqgamz'
+
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
@@ -133,3 +140,5 @@ STATIC_ROOT = BASE_DIR / "staticfiles"
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+OMDB_API_KEY = "abe79d8a"
