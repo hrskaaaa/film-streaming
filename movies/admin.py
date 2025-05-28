c92f227c00582e35feb0d2274224fbb4001920f6
Yuliia Hurska <yuliahrska07@gmail.com>
2025-05-28 16:24:41 +0300

    register created model

--- movies/admin.py
+++ movies/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from .models import Content
 
-# Register your models here.
+@admin.register(Content)
+class ContentAdmin(admin.ModelAdmin):
+    list_display = ('title', 'type', 'release_date', 'rating')
+    list_filter = ('type', 'genres')
+    search_fields = ('title', 'director', 'actors')
+    date_hierarchy = 'release_date'
+    
+    # Customize how JSONField data is displayed
+    def get_genres(self, obj):
+        return ", ".join(obj.genres) if obj.genres else ""
+    get_genres.short_description = "Genres"
+    
+    def get_actors(self, obj):
+        return ", ".join(obj.actors) if obj.actors else ""
+    get_actors.short_description = "Actors"
