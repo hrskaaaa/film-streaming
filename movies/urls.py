092938466ebdad5b3f2f31037b6dc3f1af48f6ec
Yuliia Hurska <yuliahrska07@gmail.com>
2025-05-28 16:26:58 +0300

    add urs for fetching and searching

--- movies/urls.py
+++ movies/urls.py
@@ -2,7 +2,18 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.home, name="home"),
-    path("movies/", views.movies, name="movies"),
-    path("tvshows/", views.movies, name="tvshows"),
+    path('', views.home, name='home'),
+    path('movies/', views.movies_page, name='movies'),
+    path('tvshows/', views.tvshows_page, name='tv_shows'),
+    path('anime/', views.anime_page, name='anime'),
+    path('content/<int:pk>/', views.content_detail, name='content_detail'),
+    
+    # API endpoints
+    path('api/movies/', views.fetch_movies, name='fetch_movies'),
+    path('api/tvshows/', views.fetch_tvshows, name='fetch_tvshows'),
+    path('api/anime/', views.fetch_anime, name='fetch_anime'),
+    
+    # Search endpoints
+    path('api/search/autocomplete/', views.search_autocomplete, name='search_autocomplete'),
+    path('search/', views.search_results, name='search_results'),
 ]
